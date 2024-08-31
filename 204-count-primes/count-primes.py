6459b97d5b001df75f98cc511686f2b1cd36efe4
Krishwall <98252737+Krishwall@users.noreply.github.com>
2024-08-31 19:35:34 +0530

    Time: 1867 ms (39.17%) | Memory: 74.5 MB (21.59%) - LeetSync

--- 204-count-primes/count-primes.py
+++ 204-count-primes/count-primes.py
@@ -1,14 +1,19 @@
 class Solution:
+
     def countPrimes(self, n: int) -> int:
+        def sqrt_help(sq,d,a):
+            if sq<=a:
+                return sqrt_help(sq+d,d+2,a)
+            return d
         a=[1 for i in range(n)]
         if n>2:
             a[0]=0
             a[1]=0
-            for i in range(2,int(sqrt(n))+1):
+            for i in range(2,sqrt_help(1,3,n)//2):
                 if a[i]==1:
-                    for j in range(2,n//i+1):
-                        if i*j<n:
-                            a[i*j]=0
-            return a.count(1)
+                    for j in range(i*i,n,i):
+                       
+                            a[j]=0
+            return sum(a)
         return 0
 
