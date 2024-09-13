52dcb909f106d30fe68d166e75eecce59bec2da0
Krishwall <98252737+Krishwall@users.noreply.github.com>
2024-09-13 22:26:22 +0530

    Time: 148 ms (41.92%) | Memory: 85.9 MB (45.73%) - LeetSync

--- 164-maximum-gap/maximum-gap.cpp
+++ 164-maximum-gap/maximum-gap.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
-        int n=nums.size();
+        auto n=nums.size();
         if (n<2)
         return 0;
         sort(nums.begin(),nums.end());
         int maxi=0;
-        for(int i=0;i<n-1;i++)
+        for(auto i=0;i<n-1;i++)
             {
                 maxi=max(maxi,nums[i+1]-nums[i]);
             }
