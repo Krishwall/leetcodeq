8234e10a6713d2760b6c44da4560e1ba0cb878be
Krishwall <98252737+Krishwall@users.noreply.github.com>
2024-09-29 13:41:05 +0530

    Time: 73 ms (78.41%) | Memory: 87.6 MB (97.36%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
@@ -1,17 +1,15 @@
-class Solution {
+ class Solution {
 public:
-    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
-
-        if (m*n !=original.size()) return {};
-
-        vector<vector<int>> result(m, vector<int>(n));
-        int k = 0;
-        for (int i=0;i<m;i++) {
-            for (int j=0;j<n;j++) {
-                result[i][j]=original[k++];
-            }
+    static vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        const int sz=original.size();
+        if (sz!=m*n) return {};
+        vector<vector<int>> ans(m);
+        for(int i=0; i<m; i++){
+            ans[i].assign(original.begin()+i*n, original.begin()+(i+1)*n);
         }
-
-        return result;
+        return ans;
     }
 };
+
+
+
