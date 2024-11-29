c8ce8f6a21c680867684bdcc25eb561a90264a0a
Krishwall <98252737+Krishwall@users.noreply.github.com>
2024-11-30 01:07:06 +0530

    Time: 10 ms (52.25%) | Memory: 19 MB (39.55%) - LeetSync

---
+++ 740-delete-and-earn/delete-and-earn.py
@@ -0,0 +1,17 @@
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        nums.sort()
+        n=max(nums)
+        sums=[0]*(n+1)
+        dp=[0]*(n+1)
+        for num in nums:
+            sums[num]+=num
+        dp[0]=0
+        dp[1]=sums[1]
+
+        for i in range(1,n):
+            dp[i+1]=max(sums[i+1]+dp[i-1],dp[i])
+
+        return max(dp[n-1],dp[n])
+
+        
