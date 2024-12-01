c9f7891bde6a10cc395852d0155deafc3ffed726
Krishwall <98252737+Krishwall@users.noreply.github.com>
2024-12-01 14:11:05 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (5.25%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -1,12 +1,10 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        
-        n=len(arr)
-        for i in range(n):
-            for j in range(n):
-                if i!=j:
-                    if arr[i]*2==arr[j]:
-                        return True
-                    
+    
+        mul = set()
+        for num in arr:
+            if num * 2 in mul or num / 2 in mul:
+                return True
+            mul.add(num)
         return False
 
