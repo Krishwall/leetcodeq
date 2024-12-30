3c37f77f943235af45d1ea9862b78c81586a106a
Krishwall <98252737+Krishwall@users.noreply.github.com>
2024-12-30 14:01:56 +0530

    Time: 19 ms (74.79%) | Memory: 18.8 MB (31.44%) - LeetSync

---
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        
+        for i in range(len(matrix)-2,-1,-1):
+            for j in range(len(matrix)):
+                if j==0:
+                    matrix[i][j]+=min(matrix[i+1][j],matrix[i+1][j+1])
+                elif j!=len(matrix)-1:
+                    matrix[i][j]+=min(matrix[i+1][j],matrix[i+1][j+1],matrix[i+1][j-1])
+                else:
+                    matrix[i][j]+=min(matrix[i+1][j],matrix[i+1][j-1])
+        return(min(matrix[0]))
+
+
+
+
+                
+
+            
