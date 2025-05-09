91ba133a34d63bad1c2800833940e64a428b50ca
Krishwall <98252737+Krishwall@users.noreply.github.com>
2025-05-09 23:37:01 +0530

    Time: 3 ms (17.18%) | Memory: 10.8 MB (13.23%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -5,24 +5,26 @@ vector<vector<int>> ans;
             if(k<2 || k>9 || n<1 || n>60)
             return {};
             vector<int> sub;
-            backtrack(1,k,n,sub);
+            vector<int> nums ={1,2,3,4,5,6,7,8,9};
+            backtrack(1,k,n,sub,nums);
             return ans;
         
     }
 
-    void backtrack(int idx,int k, int target,vector<int> sub)
+    void backtrack(int idx,int k, int target,vector<int> sub,vector<int> nums)
     {
         if (target==0 && k==0)
         {
             ans.push_back(sub);
             return;
         }
-        if (idx>target || idx>9)
-        return;
-        sub.push_back(idx);
-        backtrack(idx+1,k-1,target-idx, sub);
-        sub.pop_back();
-        backtrack(idx+1,k,target, sub);
+        if (k==0 || target<=0)
+          {return ;}
+         for (int i=idx;i<=9;i++) {
+            sub.push_back(i);
+            backtrack(i+1,k-1,target-i,sub,nums);
+            sub.pop_back();
+         }
         
        
 
