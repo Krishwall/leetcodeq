01d8ff5385f1b7e2a977e151c828bd20336ab08b
Krishwall <98252737+Krishwall@users.noreply.github.com>
2025-05-10 01:57:23 +0530

    Time: 86 ms (21.90%) | Memory: 88.4 MB (20.77%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -3,24 +3,24 @@ public:
     vector<vector<string>> ans;
     vector<vector<string>> partition(string s) {
         vector<string>  comb;
-        backtrack(0,s,comb);
+        backtrack(0,s,comb,s.size());
         return ans;
         
     }
 
-    void backtrack(int idx, string s, vector<string>& comb)
+    void backtrack(int idx, string s, vector<string>& comb,int len)
         {
-            if(idx==s.size())
+            if(idx==len)
             {ans.push_back(comb);
                 return;
             }
 
-        for(int i=idx;i<s.size();++i)
+        for(int i=idx;i<len;++i)
         {
             if(isPalindrome(s,idx,i))
             {
                 comb.push_back(s.substr(idx,i-idx+1));
-                backtrack(i+1,s,comb);
+                backtrack(i+1,s,comb,len);
                 comb.pop_back();
             }
         }
