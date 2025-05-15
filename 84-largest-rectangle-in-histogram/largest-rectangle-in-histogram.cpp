6512111534cfa3c90f438af5129ab82c77c8e953
Krishwall <98252737+Krishwall@users.noreply.github.com>
2025-05-15 19:12:05 +0530

    Time: 15 ms (87.95%) | Memory: 81.3 MB (78.55%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,49 +1,33 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
         int n=heights.size();
-        vector<int> nse= nsee(heights,n);
-        vector<int> pse= psee(heights,n);
-        int r,l,max_rect=0;
+        int pse,nse,ele,maxRect=0;
         for(int i=0;i<n;++i)
-        {r=nse[i];
-        l=pse[i];
-         max_rect=max(max_rect,(r-l-1)*heights[i]);
-
+            {
+                while(!st.empty() && heights[i]<heights[st.top()])
+                {
+                    ele=st.top();
+                    st.pop();
+                    nse=i;
+                    pse=st.empty()?-1:st.top();
+                    maxRect=max(maxRect,(nse-pse-1)*heights[ele]);
+                }   
+                st.push(i);
+            }
+        while(!st.empty())
+        {
+            nse=n;
+            ele=st.top();st.pop();
+            pse=st.empty()?-1:st.top();
+            maxRect=max(maxRect,(nse-pse-1)*heights[ele]);
         }
-    return max_rect;
-    }
-    vector<int> nsee(vector<int>& arr,int n ){
-        stack<int> st ;
-        vector<int> res(n); 
-        for(int r=n-1;r>=0;--r){
-            while(!st.empty() && arr[st.top()]>= arr[r])
-            st.pop();
-            
-                if(st.empty())
-                    res[r]=n;
-                else
-                    res[r]=st.top();
+        return maxRect;
 
-            st.push(r);
-            }
-        
-    return res;
-    }
-    vector<int> psee(vector<int>& arr , int n ){
-        stack<int> st ;
-        vector<int> res(n); 
-        for(int l=0;l<n;++l){
-            while(!st.empty() && arr[st.top()]> arr[l])
-            st.pop();
-            
-                if(st.empty())
-                    res[l]=-1;
-                else
-                    res[l]=st.top();
 
-            st.push(l);
-            }
-        
-    return res;}
+
+
+
+  }
 };
