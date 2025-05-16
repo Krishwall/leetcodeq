28091301a886e9b7ab07ce5299dd074d3499b924
Krishwall <98252737+Krishwall@users.noreply.github.com>
2025-05-17 01:30:05 +0530

    Time: 52 ms (95.09%) | Memory: 172 MB (96.21%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,72 +1,61 @@
 class LRUCache {
 public:
-    class Node{
-        public:
-        int key,val;
-        Node* prev;
-        Node* next;
+    queue<int>que;
+    
+    int capacity,sz;
+    
+    vector<int>keyVal,freq;
 
-        Node(int key,int val)
-        {this->key=key;
-        this->val=val;
-        }
-    };
-
-    Node* head=new Node(-1,-1);
-    Node* tail=new Node(-1,-1);
-    int cap;
-    unordered_map<int,Node*> m;
-
-    LRUCache(int capacity) {
-        cap=capacity;
-        head->next=tail;
-        tail->prev=head;
-        
-    }
-    inline void addNode(Node* newnode){
-        Node* temp=head->next;
-        newnode->next=temp;
-        newnode->prev=head;
-        head->next=newnode;
-        temp->prev=newnode;
-    }
-
-    inline void deleteNode(Node* delnode){
-        Node* prevv=delnode->prev;
-        Node* nextt=delnode->next;
-        prevv->next=nextt;
-        nextt->prev=prevv;
-    }
-    inline int get(int key) {
-
-        if (m.find(key)!=m.end()){
-            Node* resNode=m[key];
-            int ans=resNode->val;
-
-            m.erase(key);
-            deleteNode(resNode);
-            addNode(resNode);
-
-            m[key]=head->next;
-            return ans;
-        }
-        return -1;
+    LRUCache(int cap) {
+        sz=0;
+        capacity=cap;
+        keyVal.resize(10001,-1);
+        freq.resize(10001,0);
     }
     
-    inline void put(int key, int value) {
-        if (m.find(key)!=m.end()){
-            Node* curr=m[key];
-            m.erase(key);
-            deleteNode(curr);
+    int get(int key) {
+        if(keyVal[key]==-1)
+            return -1;
+            
+        que.push(key);
+        
+        freq[key]++;
+        
+        return keyVal[key];
+    }
+    
+    void put(int key, int value) {
+        if(keyVal[key]==-1)
+        {
+            while(sz>=capacity)
+            {
+                int tp=que.front();
+                
+                que.pop();
+                
+                if(freq[tp]==1)
+                {
+                    keyVal[tp]=-1;
+                    sz--;
+                }
+                
+                freq[tp]--;
+            }
+            
+            keyVal[key]=value;
+            que.push(key);
+            freq[key]=1;
+            sz++;
         }
-        if (m.size()==cap){
-            m.erase(tail->prev->key);
-            deleteNode(tail->prev);
+        else
+        {
+            keyVal[key]=value;
+            que.push(key);
+            freq[key]++;
         }
-        addNode(new Node(key,value));
-        m[key]=head->next;
     }
 };
+
 
 /**
  * Your LRUCache object will be instantiated and called as such:
