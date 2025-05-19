4b024fd9bd0527edc398858c53e2bb6d310923da
Krishwall <98252737+Krishwall@users.noreply.github.com>
2025-05-19 21:50:13 +0530

    Time: 83 ms (29.46%) | Memory: 47.6 MB (34.23%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,26 +2,27 @@ class Solution {
 public:
 int ans=0;
     int subarraySum(vector<int>& nums, int k) {
-        for(int i=0;i<nums.size();++i){
-                backtrack(i,nums,0,k);
+        map<int,int> hashMap;
+    int ans=0,sum=0;
+        for(int i=0;i<nums.size();++i)
+
+        {   sum+=nums[i];
+           if(sum==k)
+                ans++;
+
+            long long rem=sum-k;
+            if(hashMap.find(rem) != hashMap.end())
+                ans+=hashMap[rem];
+
+            
+            if(hashMap.find(sum)==hashMap.end())
+                hashMap[sum]=1;
+            else  hashMap[sum]+=1;
         }
-    
         return ans;
     }
 
-    void backtrack(int idx,vector<int>& nums , int sum,int k)
-    {
-        if(idx>=nums.size() )
-        
-            return;
-       sum+=nums[idx];
-        
-        if (sum==k)
-       { ans++;}
-            
-        
-                backtrack(idx+1,nums,sum,k);
-        }
+    
         
     
 };
