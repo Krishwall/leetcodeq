4e7c63a6a28b30c22b936acc1b60e851c3a814b5
Krishwall <98252737+Krishwall@users.noreply.github.com>
2025-05-28 16:32:47 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (65.94%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+       int m=s.length();
+       int n=t.length();
+       vector<int> hash(256,0);
+       int count=0,minl=INT_MAX;
+       int l=0,r=0,start_idx=-1; 
+       for(int i=0;i<n;++i)
+       {
+        hash[t[i]]++;
+       }
+
+       while(r<m)
+       {
+        if(hash[s[r]]>0) count+=1;
+        hash[s[r]]--;
+
+        while(count==n)
+        {   
+            if(r-l+1<minl)
+               { minl=r-l+1;
+            
+                start_idx=l;}
+            hash[s[l]]++;
+            if (hash[s[l]]>0) count-=1;
+            l++;
+
+        }
+     
+        r++;
+       }
+       return start_idx==-1?"":s.substr(start_idx,minl);
+    }
+};
