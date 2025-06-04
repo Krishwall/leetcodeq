d20c58c8bcb5457fff3c70f92c51023d636c6298
Krishwall <98252737+Krishwall@users.noreply.github.com>
2025-06-05 00:23:23 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (5.51%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -15,7 +15,7 @@ vector<int> ans;
     vector<int> preorderTraversal(TreeNode* root) {
         if(!root)
             return ans;
-        ans.push_back(root->val);
+        ans.emplace_back(root->val);
         preorderTraversal(root->left);
         preorderTraversal(root->right);
 
