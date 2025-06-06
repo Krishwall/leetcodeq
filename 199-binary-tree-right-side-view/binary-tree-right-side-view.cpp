fb0f60b9bc9c93470f26fe661f280687d6eddd1f
Krishwall <98252737+Krishwall@users.noreply.github.com>
2025-06-06 12:45:21 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (13.08%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -13,18 +13,18 @@ class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
         vector<int> ans;
-        map<int,pair<int,int>> hash;
+        map<int,int> hash;
     rightSide(root,hash,0,0);
     for(auto& a:hash)
-        ans.push_back(a.second.second);
+        ans.push_back(a.second);
     return ans;
     }
 
-    void rightSide(TreeNode* root,map<int,pair<int,int>>& hash,int idx,int depth)
+    void rightSide(TreeNode* root,map<int,int>& hash,int idx,int depth)
     {
         if(!root) return;
         if( hash.find(depth)==hash.end() )
-            {hash[depth]={idx,root->val};}
+            {hash[depth]=root->val;}
         rightSide(root->right,hash,idx+1,depth+1);
         rightSide(root->left,hash,idx-1,depth+1);
     }
