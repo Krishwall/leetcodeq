cda14991b105eda42f66d8f4c4caca580975a991
Krishwall <98252737+Krishwall@users.noreply.github.com>
2025-06-07 13:48:04 +0530

    Time: 6 ms (40.32%) | Memory: 15.3 MB (82.43%) - LeetSync

---
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,75 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+
+            unordered_map<TreeNode*,TreeNode*> parent_track;
+            markParents(root,parent_track);
+            unordered_map<TreeNode*,bool> visited;
+            queue<TreeNode*> q;
+            visited[target]=true;
+            q.push(target);
+            int curr_level=0;
+            while(!q.empty())
+            {   int sz=q.size();
+                if(curr_level++==k) break;
+
+                for(int i=0;i<sz;++i)
+                {
+                    TreeNode* curr=q.front(); q.pop();
+                    if(curr->left && !visited[curr->left]){
+                        q.push(curr->left);
+                        visited[curr->left]=true;
+                        
+                    }
+                    if(curr->right && !visited[curr->right]){
+                        q.push(curr->right);
+                        visited[curr->right]=true;
+
+                    }
+                    if(parent_track[curr] && !visited[parent_track[curr]])
+                      {  q.push(parent_track[curr]);
+                        visited[parent_track[curr]]=true;}
+                }
+
+            }
+
+            vector<int> result;
+            while(!q.empty())
+            {
+                TreeNode* curr=q.front();q.pop();
+                result.push_back(curr->val);
+            }
+            return result;
+
+    }
+
+    void markParents(TreeNode* root, unordered_map<TreeNode*,TreeNode*>& parent_track)
+    {
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            TreeNode* curr=q.front();
+            q.pop();
+            if(curr->left)
+            {
+                parent_track[curr->left]=curr;
+                q.push(curr->left);
+            }
+            if(curr->right)
+            {
+                parent_track[curr->right]=curr;
+                q.push(curr->right);
+            }
+        }
+    }
+};
