41d60f7c8f971b1a012fab6255c12284d1186e0c
Krishwall <98252737+Krishwall@users.noreply.github.com>
2025-06-08 19:16:06 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (74.25%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -16,8 +16,28 @@ public:
         {
             return 0;
         }
-       
-       return 1+countNodes(root->left) +countNodes(root->right);;
+       int lh=leftHeight(root->left);
+       int rh=rightHeight(root->right);
+
+       if(lh==rh) return (1<<lh+1)-1;
+       return 1+countNodes(root->left) +countNodes(root->right);
         
+    }
+
+    int leftHeight(TreeNode* node){
+        int h=0;
+        while(node){
+            h++;
+            node=node->left;
+        }
+        return h;
+    }
+    int rightHeight(TreeNode* node){
+        int h=0;
+        while(node){
+            h++;
+            node=node->right;
+        }
+        return h;
     }
 };
