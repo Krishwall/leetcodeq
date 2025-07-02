916dab945097c0ca8141b35ee3ff7453ddfc62a8
Krishwall <98252737+Krishwall@users.noreply.github.com>
2025-07-02 20:35:04 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (73.43%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -4,22 +4,23 @@ public:
         int n=triangle.size();
         int m=triangle[n-1].size();
 
-        vector<vector<int>> dp(n,vector<int>(m,0));
-
+        // vector<vector<int>> dp(n,vector<int>(m,0));
+        vector<int> prev(m,0);
         for(int i=0;i<n;++i)
-        {
+        {   vector<int> cur(m,0);
             for(int j=0;j<triangle[i].size();++j)
             {
-                if(i==0 && j==0) dp[i][j]=triangle[i][j];
+                if(i==0 && j==0) cur[j]=triangle[i][j];
                 else
-                {   int left=INT_MAX,right=INT_MAX,up=INT_MAX;
-                    if(j<triangle[i-1].size()) up=dp[i-1][j]+triangle[i][j];
-                    if(j>=1 ) left=dp[i-1][j-1]+triangle[i][j];
+                {   int left=INT_MAX,up=INT_MAX;
+                    if(j<triangle[i-1].size()) up=prev[j]+triangle[i][j];
+                    if(j>=1 ) left=prev[j-1]+triangle[i][j];
                   
-                    dp[i][j]=min(left,up);
+                    cur[j]=min(left,up);
                 }
             }
+            prev=cur;
         }
-        return *min_element(dp[n-1].begin(),dp[n-1].end());
+        return *min_element(prev.begin(),prev.end());
     }
 };
