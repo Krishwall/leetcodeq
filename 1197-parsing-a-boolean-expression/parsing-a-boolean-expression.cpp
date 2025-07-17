defe5efc5fe5e87a08a36aa9e29f09bf79ca08c9
Krishwall <98252737+Krishwall@users.noreply.github.com>
2025-07-17 13:35:12 +0530

    Time: 4 ms (57.09%) | Memory: 10.9 MB (78.10%) - LeetSync

---
+++ 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool parseBoolExpr(string expression) {
+        
+        stack<char> st;
+
+        for(auto a:expression)
+        {
+            if(a==',' || a=='(')
+                continue;
+
+            if(a=='&' || a=='|' || a=='t' || a=='f' || a=='!' )
+            st.push(a);
+
+            else if(a==')')
+            {
+                bool hasTrue=false;bool hasFalse=false;
+
+                while(st.top()!='!' && st.top()!='&' && st.top()!='|')
+                {
+                    char topValue=st.top();
+                    st.pop();
+                    if(topValue=='t') hasTrue=true;
+                    if(topValue=='f') hasFalse=true;
+                }
+
+                char op=st.top();
+                st.pop();
+                if(op=='!')
+                st.push(hasTrue?'f':'t');
+                else if(op=='&')
+                st.push(hasFalse?'f':'t');
+                else
+                st.push(hasTrue?'t':'f');
+            }
+        }
+
+        return st.top()=='t';
+    }
+};
