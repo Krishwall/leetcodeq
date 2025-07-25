f891249506664a0149f6aee2e2835005cc95309c
Krishwall <98252737+Krishwall@users.noreply.github.com>
2025-07-25 23:48:47 +0530

    Time: 13 ms (80.39%) | Memory: 31.9 MB (98.38%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n=mat.size();
-        int m=mat[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        vector<vector<int>> dist(n,vector<int>(m,0));
-        queue<pair<pair<int,int>,int>> q;
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<vector<int>> dist(n, vector<int>(m, 1e9)); // large initial value
 
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++){
-                if(mat[i][j]==0){
-                    q.push({{i,j},0});
-                    vis[i][j]=1;
+        // First pass: top-left to bottom-right
+        for(int i = 0; i < n; ++i) {
+            for(int j = 0; j < m; ++j) {
+                if(mat[i][j] == 0) {
+                    dist[i][j] = 0;
+                } else {
+                    if(i > 0)
+                        dist[i][j] = min(dist[i][j], dist[i - 1][j] + 1);
+                    if(j > 0)
+                        dist[i][j] = min(dist[i][j], dist[i][j - 1] + 1);
                 }
             }
         }
-        int drow[]={-1,0,1,0};
-        int dcol[]={0,1,0,-1};
-        while(!q.empty()){
-            auto [row,col]=q.front().first;
-            int steps=q.front().second;
-            q.pop();
-            dist[row][col]=steps;
 
-            for(int i=0;i<4;++i){
-                int nrow=row+drow[i];
-                int ncol=col+dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 ){
-                    vis[nrow][ncol]=1;
-                    q.push({{nrow,ncol},steps+1});
-                }
+        // Second pass: bottom-right to top-left
+        for(int i = n - 1; i >= 0; --i) {
+            for(int j = m - 1; j >= 0; --j) {
+                if(i < n - 1)
+                    dist[i][j] = min(dist[i][j], dist[i + 1][j] + 1);
+                if(j < m - 1)
+                    dist[i][j] = min(dist[i][j], dist[i][j + 1] + 1);
             }
         }
+
         return dist;
     }
 };
