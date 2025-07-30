2ccd3723ee93237bd814577acbba8f2a85627bf1
Krish <98252737+Krishwall@users.noreply.github.com>
2025-07-30 23:59:16 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (33.34%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,51 +1,19 @@
-
 class Solution {
-    struct Node{
-        int fl=false;
-        Node* links[26];
-        Node* get(char ch)
-        {
-            return links[ch-'a'];
-        }
-        bool containsKey(char ch){ return links[ch-'a']!=NULL;}
-        void put(char ch,Node* node) {links[ch-'a']=node;}
-
-    };
-
-    class Trie{
-        public:
-        Node* root;
-        Trie()
-        {
-            root=new Node();
-        }
-        int insert(string word){
-            Node* node=root;
-            int cnt=0;
-            for(int i=0;i<word.size();++i)
-            {
-                if(!node->containsKey(word[i])){
-                    node->put(word[i],new Node());
-                }
-                else{cnt++;}
-                node=node->get(word[i]);
-            }
-            return cnt;
-        }
-
-        
-    };
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        Trie trie;
-        int count=INT_MAX;
-       if(strs.size()==1) return strs[0];
-       trie.insert(strs[0]);
-        for(int i=1;i<strs.size();++i)
-        {   
-            
-            count=min(count,trie.insert(strs[i]));
+        if (strs.empty()) return "";
+
+        string prefix = strs[0];
+
+        for (int i = 1; i < strs.size(); ++i) {
+            int j = 0;
+            while (j < prefix.size() && j < strs[i].size() && prefix[j] == strs[i][j]) {
+                j++;
+            }
+            prefix = prefix.substr(0, j);  // shrink the prefix
+            if (prefix.empty()) break;     // early exit
         }
-        return strs[0].substr(0,count);
+
+        return prefix;
     }
 };
