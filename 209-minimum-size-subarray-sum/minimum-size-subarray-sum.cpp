6982e5fa323d82da1df685cc6c98a7a0f651c587
Krish <98252737+Krishwall@users.noreply.github.com>
2025-12-06 00:28:57 +0530

    Time: 0 ms (100.00%) | Memory: 32.2 MB (37.54%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,38 +1,22 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
+        int n = nums.size();
+        int left = 0;
+        int sum = 0;
+        int ans = INT_MAX;
         
-        int low=1, high=nums.size(),mn=0;
-        int n=high;
-        while(low<=high){
-            int mid=(low+high)/2;
-
-            if(windowFind(mid,nums,target,n)){
-                high=mid-1;
-                mn=mid;
-            }else low=mid+1;
-        }
-        return mn;
-    }
-
-    bool windowFind(int size, vector<int>& nums,int& target,int& n)
-    {
-        int sum=0;int i=0,j=0;
-        int mx=INT_MIN;
-        while(j<n)
-        {
-            sum+=nums[j];
-            if(j-i+1==size){
-                mx=max(sum,mx);
-                sum-=nums[i];
-                i++;
-                }
-                j++;
+        for (int right = 0; right < n; right++) {
+            sum += nums[right];
+            
+            
+            while (sum >= target) {
+                ans = min(ans, right - left + 1);
+                sum -= nums[left];
+                left++;
             }
-
-        if(mx>=target)
-        return true;
-        return false;
         }
-    
+        
+        return (ans == INT_MAX ? 0 : ans);
+    }
 };
