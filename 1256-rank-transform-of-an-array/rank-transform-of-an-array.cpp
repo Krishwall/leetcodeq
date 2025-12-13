a8b8d1bc4d4f242322336a3d20d978f5003a18cf
Krish <98252737+Krishwall@users.noreply.github.com>
2025-12-13 20:02:48 +0530

    Time: 35 ms (87.30%) | Memory: 45.2 MB (29.32%) - LeetSync

---
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        
+        vector<int> sortedarr=arr;
+        unordered_map<int,int> hash;
+        sort(arr.begin(),arr.end());
+        int k=1;
+        for (auto num : arr)
+        {
+            if(hash.find(num)==hash.end())
+            {
+                hash[num]=k;
+                k++;
+            }
+        }
+        vector<int> ans;
+        for (auto a:sortedarr)
+        {
+            ans.push_back(hash[a]);
+        }
+        return ans;
+    }
+};
