4a221a3298dd288b6b12221126fed4e8d5d9a4dc
Krish <98252737+Krishwall@users.noreply.github.com>
2025-12-13 20:05:50 +0530

    Time: 31 ms (92.25%) | Memory: 39.8 MB (91.13%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
+    struct cmp{
+        bool operator()(pair<int,int>&a,pair<int,int>&b){
+            return a.first>b.first;
+        }
+    };
     vector<int> arrayRankTransform(vector<int>& arr) {
-        
-        vector<int> sortedarr=arr;
-        unordered_map<int,int> hash;
-        sort(arr.begin(),arr.end());
-        int k=1;
-        for (auto& num : arr)
-        {
-            if(hash.find(num)==hash.end())
-            {
-                hash[num]=k;
-                k++;
+        if(arr.size()==0)return {};
+        priority_queue<pair<int,int>,vector<pair<int,int>>,cmp>pq;
+        for(int i=0;i<arr.size();i++){
+            pq.push({arr[i],i});
+        }
+        vector<int>ans(arr.size());
+        int m=pq.top().first;
+        int i=1;
+        ans[pq.top().second]=i;
+        pq.pop();
+        while(!pq.empty()){
+            if(m==pq.top().first){
+                ans[pq.top().second]=i;
+                pq.pop();
+            }
+            else{
+                m=pq.top().first;
+                ans[pq.top().second]=++i;
+                pq.pop();
             }
         }
-        vector<int> ans;
-        for (auto& a:sortedarr)
-        {
-            ans.push_back(hash[a]);
-        }
         return ans;
+
     }
 };
