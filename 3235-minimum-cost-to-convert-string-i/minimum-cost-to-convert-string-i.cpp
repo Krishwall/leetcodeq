ba9aa661010ffd259e3069b42592faa3a176ebf3
Krish <98252737+Krishwall@users.noreply.github.com>
2026-01-29 15:49:20 +0530

    Time: 18 ms (93.30%) | Memory: 93.7 MB (99.77%) - LeetSync

--- 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
@@ -20,7 +20,7 @@ class Solution {
     }
     }
 public:
-    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+    long long minimumCost(string& source, string& target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
 
         
         vector<vector<int>> graph(26,vector<int>(26,1e8));
