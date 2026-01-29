908beb74bd8cfe8edaafb072d73c61c34d48e93f
Krish <98252737+Krishwall@users.noreply.github.com>
2026-01-29 20:33:57 +0530

    Time: 4 ms (44.61%) | Memory: 15.1 MB (23.29%) - LeetSync

---
+++ 71-simplify-path/simplify-path.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string> st;
+        string token;
+        
+        for (int i = 0; i <= path.size(); i++) {
+            if (i == path.size() || path[i] == '/') {
+                if (token == "..") {
+                    if (!st.empty()) st.pop();
+                } 
+                else if (!token.empty() && token != ".") {
+                    st.push(token);
+                }
+                token.clear();
+            } 
+            else {
+                token += path[i];
+            }
+        }
+
+        string result;
+        while (!st.empty()) {
+            result = "/" + st.top() + result;
+            st.pop();
+        }
+
+        return result.empty() ? "/" : result;
+    }
+};
