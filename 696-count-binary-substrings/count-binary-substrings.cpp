34556519795af84da1e1951d5474f53e495616e4
Krish <98252737+Krishwall@users.noreply.github.com>
2026-02-19 23:15:19 +0530

    Time: 3 ms (67.83%) | Memory: 13 MB (82.66%) - LeetSync

--- 696-count-binary-substrings/count-binary-substrings.cpp
+++ 696-count-binary-substrings/count-binary-substrings.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    int countBinarySubstrings(string& s) {
-        vector<int> freq;
-        int count=1;
-        for(int i=1;i<s.size();i++){
-            if(s[i]==s[i-1])
-            count++;
-            else{
-                freq.push_back(count);
-                count=1;
-            }
-            
+    int countBinarySubstrings(string s) {
+    int ans = 0, previous = 0, current = 1;
+    for(int i = 1 ; i < s.size() ; i++){
+        if(s[i] == s[i-1]) current++;
+        else{
+            previous = current;
+            current = 1;
         }
-        freq.push_back(count);
-        int ans=0;
-        for(int i=1;i<freq.size();i++)
-        {
-            ans+=min(freq[i-1],freq[i]);
-        }
-
-        return ans;
+        if(current <= previous) ans++;
+    }
+    return ans;
+        
+       
     }
 };
