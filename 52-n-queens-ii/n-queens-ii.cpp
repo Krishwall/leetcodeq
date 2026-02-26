d18781f997cec591474ee17ee0bfb5fd6e6da4b2
Krish <98252737+Krishwall@users.noreply.github.com>
2026-02-26 22:00:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (58.35%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.cpp
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -1,48 +1,36 @@
 class Solution {
 public:
-int ans=0;
-    int totalNQueens(int n) {
-        
-        vector<string> board(n);
-        string s(n,'.');
-        for(int i=0;i<n;++i)
-            {
-                board[i]=s;
+    int ans = 0;
 
-            }
-            vector<int> leftRow(n,0), upperDiagonal(2*n-1,0), lowerDiagonal(2*n-1,0);
-            putQueenBacktrack(0,board,leftRow,upperDiagonal,lowerDiagonal,n);
+    int totalNQueens(int n) {
+        vector<int> leftRow(n, 0), upperDiagonal(2*n-1, 0), lowerDiagonal(2*n-1, 0);
+        solve(0, leftRow, upperDiagonal, lowerDiagonal, n);
         return ans;
     }
 
-    void putQueenBacktrack(int col, vector<string>& board,  vector<int>& leftRow, vector<int>& upperDiagonal, vector<int>& lowerDiagonal, int n)
+    void solve(int col, vector<int>& leftRow, vector<int>& upperDiagonal,
+               vector<int>& lowerDiagonal, int n)
     {
-       if(col==n)
-       {
-        ans++;
-        return ;
-       }
-       for(int row=0;row<n;++row)
-       
-        if(leftRow[row]==0 && lowerDiagonal[row+col]==0 && upperDiagonal[n-1 +(col-row)]==0)
-        {
-
-            board[row][col]='Q';
-            leftRow[row]=1;
-            lowerDiagonal[row+col]=1;
-            upperDiagonal[n-1 + (col-row)]=1;
-
-            putQueenBacktrack(col+1,board ,leftRow,upperDiagonal,lowerDiagonal,n );
-
-            board[row][col]='.';
-            leftRow[row]=0;
-            lowerDiagonal[row+col]=0;
-            upperDiagonal[n-1 + (col-row)]=0;
+        if (col == n) {
+            ans++;
+            return;
         }
-       
-        
 
+        for (int row = 0; row < n; row++) {
+            if (!leftRow[row] &&
+                !lowerDiagonal[row + col] &&
+                !upperDiagonal[n - 1 + col - row])
+            {
+                leftRow[row] = 1;
+                lowerDiagonal[row + col] = 1;
+                upperDiagonal[n - 1 + col - row] = 1;
 
+                solve(col + 1, leftRow, upperDiagonal, lowerDiagonal, n);
 
+                leftRow[row] = 0;
+                lowerDiagonal[row + col] = 0;
+                upperDiagonal[n - 1 + col - row] = 0;
+            }
+        }
     }
 };
