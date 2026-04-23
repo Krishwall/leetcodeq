1c6968ed15418c18d2b88f4b831a58faef498cd3
Krish <98252737+Krishwall@users.noreply.github.com>
2026-04-23 14:44:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (67.18%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -1,26 +1,37 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-    unordered_map<char,int> lastIndex;
+    // unordered_map<char,int> lastIndex;
+    vector<int> lastIndex(26,0);
+    vector<int> firstIndex(26,0);
+    int partitionEnd=0,partitionStart=0;
+
     int n=s.length();
-    for(int i=n-1;i>=0;i--)
+    for(int i=0;i<n;i++)
     {
-        if(lastIndex.find(s[i])==lastIndex.end())
-            lastIndex[s[i]]=i;
+        
+            lastIndex[s[i]-'a']=i;
     }
 
-    int partitionEnd=0,newStart=0;
     vector<int> ans;
-    for(int i=0;i<n;i++)
+    for(int i=0;i<s.length();i++)
     {   
-        partitionEnd=max(lastIndex[s[i]],partitionEnd);
-        if(i==partitionEnd)
+        if(!firstIndex[s[i]-'a'])
+            firstIndex[s[i]-'a']=i;
+
+        if(partitionEnd<firstIndex[s[i]-'a'])
         {
-            ans.push_back(i-newStart+1);
-            newStart=i+1;
+            ans.push_back(partitionEnd-partitionStart+1);
+            partitionStart=i;
+            partitionEnd=i;
         }
-        
+
+        partitionEnd=max(partitionEnd,lastIndex[s[i]-'a']);
+
+
     }
+        if(partitionEnd-partitionStart+1>0)
+            ans.push_back(partitionEnd-partitionStart+1);
         return ans;
     
     }
